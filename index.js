3b39ef0a7eccf44f68cd5c6e933dd78f3f514a24
carly <carly@noukod.com>
2019-02-27 14:32:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob' ;
+const leastFavoriteCustomer = 'certil';
+
+function upperCaseCustomerName()
+{
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer()
+{
+  leastFavoriteCustomer = 'HI';
+}
